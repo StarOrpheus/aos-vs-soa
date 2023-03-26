601448c597bb22e2682afa8a8e7a119c87c66ddc
Zakhar Koval <Zakhar.Koval@jetbrains.com>
2023-03-26 21:05:14 +0300

    Added more benchmarks & results

--- README.md
+++ README.md
@@ -6,23 +6,28 @@ Comparison of vector<MyStruct> (MyStruct just a wrapper for int) vs vector<int> 
 
 #### i9-9980HK - GCC toolchain with stdlibc++
 
-| Benchmark                                  | Time      | CPU       | Iterations | Throughput |
-|--------------------------------------------|-----------|-----------|------------|------------|
-| BM_copy_vector_of_structures               | 530217 ns | 529458 ns | 1219       | 1.84446G/s |
-| BM_copy_vector_of_structures_range_reserve | 835599 ns | 833853 ns | 839        | 1.17114G/s |
-| BM_copy_vector_of_structures_std_copy      | 808617 ns | 807561 ns | 818        | 1.20927G/s |
-| BM_copy_vector_of_ints                     | 375849 ns | 375408 ns | 1709       | 2.60134G/s |
-| BM_copy_vector_of_ints_range_reserve       | 999301 ns | 997473 ns | 691        | 1002.53M/s |
-| BM_copy_vector_of_ints_std_copy            | 752010 ns | 751239 ns | 887        | 1.29994G/s |
+| Benchmark                                           | Time      | CPU       | Iterations | Throughput |
+|-----------------------------------------------------|-----------|-----------|------------|------------|
+| BM_copy_vector_of_structures                        | 558006 ns | 557263 ns | 1145       | 1.75243G/s |
+| BM_copy_vector_of_structures_memcpy                 | 581709 ns | 580550 ns | 1280       | 1.68213G/s |
+| BM_copy_vector_of_structures_range_reserve          | 989279 ns | 988169 ns | 631        | 1011.97M/s |
+| BM_copy_vector_of_structures_std_copy               | 825428 ns | 823923 ns | 863        | 1.18526G/s |
+| BM_copy_vector_of_structures_std_copy_back_inserter | 577019 ns | 575875 ns | 1181       | 1.69579G/s |
+| BM_copy_vector_of_ints                              | 388065 ns | 387678 ns | 1842       | 2.51901G/s |
+| BM_copy_vector_of_ints_memcpy                       | 384529 ns | 384104 ns | 1677       | 2.54244G/s |
+| BM_copy_vector_of_ints_range_reserve                | 853172 ns | 851390 ns | 814        | 1.14702G/s |
+| BM_copy_vector_of_ints_std_copy                     | 408835 ns | 407615 ns | 1806       | 2.39579G/s |
+| BM_copy_vector_of_ints_std_copy_back_inserter       | 805369 ns | 804680 ns | 876        | 1.2136G/s  |
+
 
 
 #### i9-9980HK - Clang toolchain with libc++
 
-| Benchmark                                  | Time       | CPU        | Iterations | Throughput |
-|--------------------------------------------|------------|------------|------------|------------|
-| BM_copy_vector_of_structures               | 229061 ns  | 228511 ns  | 3068       | 4.27359G/s |
-| BM_copy_vector_of_structures_range_reserve | 1135954 ns | 1133798 ns | 627        | 881.992M/s |
-| BM_copy_vector_of_structures_std_copy      | 1108562 ns | 1107114 ns | 642        | 903.25M/s  |
-| BM_copy_vector_of_ints                     | 219725 ns  | 219468 ns  | 2939       | 4.44969G/s |
-| BM_copy_vector_of_ints_range_reserve       | 1140654 ns | 1138533 ns | 619        | 878.323M/s |
-| BM_copy_vector_of_ints_std_copy            | 1118975 ns | 1117082 ns | 635        | 895.189M/s |
+| Benchmark                                           | Time       | CPU        | Iterations | Thoughput  |
+|-----------------------------------------------------|------------|------------|------------|------------|
+| BM_copy_vector_of_structures                        | 251962 ns  | 251667 ns  | 2481       | 3.88037G/s |
+| BM_copy_vector_of_structures_memcpy                 | 424831 ns  | 424186 ns  | 1628       | 2.30221G/s |
+| BM_copy_vector_of_structures_range_reserve          | 1154824 ns | 1153022 ns | 615        | 867.286M/s |
+| BM_copy_vector_of_structures_std_copy               | 1199840 ns | 1197704 ns | 635        | 834.931M/s |
+| BM_copy_vector_of_structures_std_copy_back_inserter | 435286 ns  | 434430 ns  | 1662       | 2.24792G/s |
+| BM_copy_vector_of_ints                              | 254127 ns  | 253718 ns  | 2823       | 3.84901G/s |
--- main.cpp
+++ main.cpp
@@ -1,135 +1,204 @@
+#include <algorithm>
 #include <iostream>
 #include <random>
+#include <cstring>
 
 #include <benchmark/benchmark.h>
 
 constexpr auto N = 1 << 20;
 
 struct MyStruct {
-    int x;
+  int x;
 };
 
 namespace {
-    void BM_copy_vector_of_structures(benchmark::State &State) {
-        std::vector<MyStruct> Data;
-        std::mt19937 RandomD(std::random_device{}());
-        std::uniform_int_distribution<int> D;
-        for (size_t I = 0; I < N; ++I) {
-            Data.push_back({D(RandomD)});
-        }
+void BM_copy_vector_of_structures(benchmark::State &State) {
+  std::vector<MyStruct> Data;
+  std::mt19937 RandomD(std::random_device{}());
+  std::uniform_int_distribution<int> D;
+  for (size_t I = 0; I < N; ++I) {
+    Data.push_back({D(RandomD)});
+  }
 
-        for (auto &&_: State) {
-            std::vector<MyStruct> Other(Data.size());
+  for (auto &&_ : State) {
+    std::vector<MyStruct> Other(Data.size());
 
-            for (size_t I = 0; I < Data.size(); ++I) {
-                Other[I] = Data[I];
-            }
-
-            benchmark::DoNotOptimize(Other);
-        }
-        State.SetBytesProcessed(State.iterations() * Data.size());
+    for (size_t I = 0; I < Data.size(); ++I) {
+      Other[I] = Data[I];
     }
 
-    void BM_copy_vector_of_structures_range_reserve(benchmark::State &State) {
-        std::vector<MyStruct> Data;
-        std::mt19937 RandomD(std::random_device{}());
-        std::uniform_int_distribution<int> D;
-        for (size_t I = 0; I < N; ++I) {
-            Data.push_back({D(RandomD)});
-        }
-
-        for (auto &&_: State) {
-            std::vector<MyStruct> Other;
-            Other.reserve(Data.size());
-            for (auto x: Data)
-                Other.push_back(x);
-
-            benchmark::DoNotOptimize(Other);
-        }
-        State.SetBytesProcessed(State.iterations() * Data.size());
-    }
-
-    void BM_copy_vector_of_structures_std_copy(benchmark::State &State) {
-        std::vector<MyStruct> Data;
-        std::mt19937 RandomD(std::random_device{}());
-        std::uniform_int_distribution<int> D;
-        for (size_t I = 0; I < N; ++I) {
-            Data.push_back({D(RandomD)});
-        }
-
-        for (auto &&_: State) {
-            std::vector<MyStruct> Other;
-            Other.reserve(Data.size());
-            std::copy(Data.begin(), Data.end(), std::back_inserter(Other));
-            benchmark::DoNotOptimize(Other);
-        }
-        State.SetBytesProcessed(State.iterations() * Data.size());
-    }
-
-    void BM_copy_vector_of_ints(benchmark::State &State) {
-        std::vector<int> Data;
-        std::mt19937 RandomD(std::random_device{}());
-        std::uniform_int_distribution<int> D;
-        for (size_t I = 0; I < N; ++I) {
-            Data.push_back({D(RandomD)});
-        }
-
-        for (auto &&_: State) {
-            std::vector<int> Other(Data.size());
-
-            for (size_t I = 0; I < Data.size(); ++I) {
-                Other[I] = Data[I];
-            }
-
-            benchmark::DoNotOptimize(Other);
-        }
-        State.SetBytesProcessed(State.iterations() * Data.size());
-    }
-
-    void BM_copy_vector_of_ints_range_reserve(benchmark::State &State) {
-        std::vector<int> Data;
-        std::mt19937 RandomD(std::random_device{}());
-        std::uniform_int_distribution<int> D;
-        for (size_t I = 0; I < N; ++I) {
-            Data.push_back({D(RandomD)});
-        }
-
-        for (auto &&_: State) {
-            std::vector<int> Other;
-            Other.reserve(Data.size());
-            for (auto x: Data)
-                Other.push_back(x);
-
-            benchmark::DoNotOptimize(Other);
-        }
-        State.SetBytesProcessed(State.iterations() * Data.size());
-    }
-
-    void BM_copy_vector_of_ints_std_copy(benchmark::State &State) {
-        std::vector<int> Data;
-        std::mt19937 RandomD(std::random_device{}());
-        std::uniform_int_distribution<int> D;
-        for (size_t I = 0; I < N; ++I) {
-            Data.push_back({D(RandomD)});
-        }
-
-        for (auto &&_: State) {
-            std::vector<int> Other;
-            Other.reserve(Data.size());
-            std::copy(Data.begin(), Data.end(), std::back_inserter(Other));
-            benchmark::DoNotOptimize(Other);
-
-        }
-        State.SetBytesProcessed(State.iterations() * Data.size());
-    }
+    benchmark::DoNotOptimize(Other);
+  }
+  State.SetBytesProcessed(State.iterations() * Data.size());
 }
 
+void BM_copy_vector_of_structures_memcpy(benchmark::State &State) {
+  std::vector<MyStruct> Data;
+  std::mt19937 RandomD(std::random_device{}());
+  std::uniform_int_distribution<int> D;
+  for (size_t I = 0; I < N; ++I) {
+    Data.push_back({D(RandomD)});
+  }
+
+  for (auto &&_ : State) {
+    std::vector<MyStruct> Other(Data.size());
+    std::memcpy(Other.data(), Data.data(), Data.size() * sizeof(MyStruct));
+    benchmark::DoNotOptimize(Other);
+  }
+  State.SetBytesProcessed(State.iterations() * Data.size());
+}
+
+void BM_copy_vector_of_structures_range_reserve(benchmark::State &State) {
+  std::vector<MyStruct> Data;
+  std::mt19937 RandomD(std::random_device{}());
+  std::uniform_int_distribution<int> D;
+  for (size_t I = 0; I < N; ++I) {
+    Data.push_back({D(RandomD)});
+  }
+
+  for (auto &&_ : State) {
+    std::vector<MyStruct> Other;
+    Other.reserve(Data.size());
+    for (auto x : Data)
+      Other.push_back(x);
+
+    benchmark::DoNotOptimize(Other);
+  }
+  State.SetBytesProcessed(State.iterations() * Data.size());
+}
+
+void BM_copy_vector_of_structures_std_copy_back_inserter(benchmark::State &State) {
+  std::vector<MyStruct> Data;
+  std::mt19937 RandomD(std::random_device{}());
+  std::uniform_int_distribution<int> D;
+  for (size_t I = 0; I < N; ++I) {
+    Data.push_back({D(RandomD)});
+  }
+
+  for (auto &&_ : State) {
+    std::vector<MyStruct> Other(Data.size());
+    std::copy(Data.begin(), Data.end(), Other.begin());
+    benchmark::DoNotOptimize(Other);
+  }
+  State.SetBytesProcessed(State.iterations() * Data.size());
+}
+
+void BM_copy_vector_of_structures_std_copy(benchmark::State &State) {
+  std::vector<MyStruct> Data;
+  std::mt19937 RandomD(std::random_device{}());
+  std::uniform_int_distribution<int> D;
+  for (size_t I = 0; I < N; ++I) {
+    Data.push_back({D(RandomD)});
+  }
+
+  for (auto &&_ : State) {
+    std::vector<MyStruct> Other;
+    Other.reserve(Data.size());
+    std::copy(Data.begin(), Data.end(), std::back_inserter(Other));
+    benchmark::DoNotOptimize(Other);
+  }
+  State.SetBytesProcessed(State.iterations() * Data.size());
+}
+
+void BM_copy_vector_of_ints(benchmark::State &State) {
+  std::vector<int> Data;
+  std::mt19937 RandomD(std::random_device{}());
+  std::uniform_int_distribution<int> D;
+  for (size_t I = 0; I < N; ++I) {
+    Data.push_back({D(RandomD)});
+  }
+
+  for (auto &&_ : State) {
+    std::vector<int> Other(Data.size());
+
+    for (size_t I = 0; I < Data.size(); ++I) {
+      Other[I] = Data[I];
+    }
+
+    benchmark::DoNotOptimize(Other);
+  }
+  State.SetBytesProcessed(State.iterations() * Data.size());
+}
+
+void BM_copy_vector_of_ints_memcpy(benchmark::State &State) {
+  std::vector<int> Data;
+  std::mt19937 RandomD(std::random_device{}());
+  std::uniform_int_distribution<int> D;
+  for (size_t I = 0; I < N; ++I) {
+    Data.push_back({D(RandomD)});
+  }
+
+  for (auto &&_ : State) {
+    std::vector<int> Other(Data.size());
+    std::memcpy(Other.data(), Data.data(), Data.size() * sizeof(int));
+    benchmark::DoNotOptimize(Other);
+  }
+  State.SetBytesProcessed(State.iterations() * Data.size());
+}
+
+void BM_copy_vector_of_ints_range_reserve(benchmark::State &State) {
+  std::vector<int> Data;
+  std::mt19937 RandomD(std::random_device{}());
+  std::uniform_int_distribution<int> D;
+  for (size_t I = 0; I < N; ++I) {
+    Data.push_back({D(RandomD)});
+  }
+
+  for (auto &&_ : State) {
+    std::vector<int> Other;
+    Other.reserve(Data.size());
+    for (auto x : Data)
+      Other.push_back(x);
+
+    benchmark::DoNotOptimize(Other);
+  }
+  State.SetBytesProcessed(State.iterations() * Data.size());
+}
+
+void BM_copy_vector_of_ints_std_copy_back_inserter(benchmark::State &State) {
+  std::vector<int> Data;
+  std::mt19937 RandomD(std::random_device{}());
+  std::uniform_int_distribution<int> D;
+  for (size_t I = 0; I < N; ++I) {
+    Data.push_back({D(RandomD)});
+  }
+
+  for (auto &&_ : State) {
+    std::vector<int> Other;
+    Other.reserve(Data.size());
+    std::copy(Data.begin(), Data.end(), std::back_inserter(Other));
+    benchmark::DoNotOptimize(Other);
+  }
+  State.SetBytesProcessed(State.iterations() * Data.size());
+}
+
+void BM_copy_vector_of_ints_std_copy(benchmark::State &State) {
+  std::vector<int> Data;
+  std::mt19937 RandomD(std::random_device{}());
+  std::uniform_int_distribution<int> D;
+  for (size_t I = 0; I < N; ++I) {
+    Data.push_back({D(RandomD)});
+  }
+
+  for (auto &&_ : State) {
+    std::vector<int> Other(Data.size());
+    std::copy(Data.begin(), Data.end(), Other.begin());
+    benchmark::DoNotOptimize(Other);
+  }
+  State.SetBytesProcessed(State.iterations() * Data.size());
+}
+} // namespace
+
 BENCHMARK(BM_copy_vector_of_structures);
+BENCHMARK(BM_copy_vector_of_structures_memcpy);
 BENCHMARK(BM_copy_vector_of_structures_range_reserve);
 BENCHMARK(BM_copy_vector_of_structures_std_copy);
+BENCHMARK(BM_copy_vector_of_structures_std_copy_back_inserter);
 
 BENCHMARK(BM_copy_vector_of_ints);
+BENCHMARK(BM_copy_vector_of_ints_memcpy);
 BENCHMARK(BM_copy_vector_of_ints_range_reserve);
 BENCHMARK(BM_copy_vector_of_ints_std_copy);
+BENCHMARK(BM_copy_vector_of_ints_std_copy_back_inserter);
 
 BENCHMARK_MAIN();
